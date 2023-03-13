136019d434e2e84e1258fcdd189b222a33b7ad2c
Alibek Beket <alibek1beket@gmail.com>
2023-03-14 00:21:57 +0600

    изменил функцию для редактирования товара ProductUpdateView

--- app/e_shop/urls.py
+++ app/e_shop/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('product/', ProductView.as_view(), name='products_list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('product/add/', ProductAddView.as_view(), name='product_add'),
-    path('product/<int:pk>/update', product_update_view, name='product_update'),
+    path('product/<int:pk>/update', ProductUpdateView.as_view(), name='product_update'),
     path('product/<int:pk>/delete', product_delete_view, name='product_delete'),
     path('product/<int:pk>/confirm_delete', product_confirm_delete_view, name='product_confirm_delete'),
 ]
--- app/e_shop/views/product.py
+++ app/e_shop/views/product.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.utils.http import urlencode
 from django.views import View
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from e_shop.models import *
 
 from e_shop.forms import *
@@ -61,39 +61,13 @@ class ProductAddView(CreateView):
         return reverse('product_detail', kwargs={'pk': self.object.pk})
 
 
-def product_update_view(request, pk):
-    product_info = get_object_or_404(Product, pk=pk)
-    product_info_dict = {
-        'product_name': product_info.product_name,
-        'product_description': product_info.product_description,
-        'product_image': product_info.product_image,
-        'category': product_info.category,
-        'the_rest_of_the_goods': product_info.the_rest_of_the_goods,
-        'price': product_info.price,
-    }
-    form = ProductForm(data=product_info_dict)
-    if not request.POST:
-        context = {
-            'form': form,
-            'product': product_info
-        }
-        return render(request, 'product_update_page.html', context=context)
-    form = ProductForm(data=request.POST)
-    if not form.is_valid():
-        context = {
-            'form': form,
-            'product': product_info
-        }
-        return render(request, 'product_update_page.html', context=context)
-    else:
-        product_info.product_name = request.POST.get('product_name')
-        product_info.product_description = request.POST.get('product_description')
-        product_info.product_image = request.POST.get('product_image')
-        product_info.category = request.POST.get('category')
-        product_info.the_rest_of_the_goods = request.POST.get('the_rest_of_the_goods')
-        product_info.price = request.POST.get('price')
-        product_info.save()
-        return redirect('product_detail', pk=product_info.pk)
+class ProductUpdateView(UpdateView):
+    template_name = 'product_update_page.html'
+    model = Product
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse('product_detail', kwargs={'pk': self.object.pk})
 
 
 def product_delete_view(request, pk):
