fc4a385af5fda3f8a3ce6b7e42ed870d8696d4aa
Alibek Beket <alibek1beket@gmail.com>
2023-03-14 03:33:51 +0600

    исправил ошибки при добавлении товаров в корзину(чтоб нельзя было добавить товаров больше чем остаток в магазине)

--- app/e_shop/views/item_in_cart.py
+++ app/e_shop/views/item_in_cart.py
@@ -10,13 +10,16 @@ class ProductCartAddView(TemplateView):
 
     def post(self, request, *args, **kwargs):
         products = ItemInCart.objects.all()
+        product_in_cart = Product.objects.get(id=kwargs['pk'])
         for product in products:
-            if product.product_pk == Product.objects.get(id=kwargs['pk']):
+            if product.product_pk == product_in_cart:
+                if product.count == product_in_cart.the_rest_of_the_goods or product_in_cart.the_rest_of_the_goods == 0:
+                    return redirect('products_list')
                 product.count += 1
                 product.save()
                 return redirect('products_list')
         product = ItemInCart()
-        product.product_pk = Product.objects.get(id=kwargs['pk'])
+        product.product_pk = product_in_cart
         product.count = 1
         product.save()
         return redirect('products_list')
