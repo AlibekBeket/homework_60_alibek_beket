c6a419bc0b9d3a73de35448dc4b076a0df2ef026
Alibek Beket <alibek1beket@gmail.com>
2023-03-14 16:35:46 +0600

    обновил фикстуру и изменил название файла для нахождения товара по имени или описанию

--- app/e_shop/views/product.py
+++ app/e_shop/views/product.py
@@ -2,8 +2,8 @@ from django.db.models import Q
 from django.urls import reverse, reverse_lazy
 from django.utils.http import urlencode
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from e_shop.models import *
-from e_shop.forms import *
+from e_shop.models import Product
+from e_shop.forms import SearchForm, ProductForm
 
 
 class ProductView(ListView):
@@ -19,13 +19,12 @@ class ProductView(ListView):
         self.search_value = self.get_search_value()
         return super().get(request, *args, **kwargs)
 
-    def get_search_form(self):
-        return SearchForm(self.request.GET)
-
-    def get_search_value(self):
-        if self.form.is_valid():
-            return self.form.cleaned_data['search']
-        return None
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form']: self.form
+        if self.search_value:
+            context['query']: urlencode({'search': self.search_value})
+        return context
 
     def get_queryset(self):
         queryset = super().get_queryset().exclude(the_rest_of_the_goods=0)
@@ -34,12 +33,13 @@ class ProductView(ListView):
             queryset = queryset.filter(query)
         return queryset
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(object_list=object_list, **kwargs)
-        context['form']: self.form
-        if self.search_value:
-            context['query']: urlencode({'search': self.search_value})
-        return context
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
 
 
 class ProductDetailView(DetailView):
